a288a117cef8529687ae6d01d5300e2f2b1ce3e1
nayankela <nayankela09@gmail.com>
2020-10-06 14:00:27 +0530

    done

---
+++ superwars/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BodyComponent } from './body/body.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BodyComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
